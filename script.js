ec844630ecdc38c9eccf9bd1aeffd7a0840609d3
Thinh <nguyenqt1801@gmail.com>
2023-08-04 09:49:34 +0700

    x

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 //Declaring and initializing variables
+function monthDiff(d1, d2) {
+  var months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  return months <= 0 ? 0 : months;
+}
 let submit = document.getElementById("submit");
 let output = document.getElementById("output");
 let date1 = new Date("2022-10-30")
@@ -8,7 +15,7 @@ document.getElementById("date-2").valueAsDate = new Date();
 let timeDifference = date2.getTime() - date1.getTime();
 let diff = new Date(timeDifference)
 let yearDifference = diff.getFullYear() - 1970;
-let monthDifference = diff.getMonth() ;
+let monthDifference = monthDiff(date1,date2) ;
 let dayDifference =  Math.round(timeDifference / (1000 * 3600 * 24));
 
 // let dayDifference = Math.round(timeDifference / (1000 * 3600 * 24));
@@ -25,7 +32,7 @@ submit.addEventListener("click", () => {
      timeDifference = date2.getTime() - date1.getTime();
      diff = new Date(timeDifference)
      yearDifference = diff.getFullYear() - 1970;
-     monthDifference = diff.getMonth() ;
+    monthDifference = monthDiff(date1,date2) ;
      dayDifference = Math.round(timeDifference / (1000 * 3600 * 24));
     output.innerHTML = `Difference between the day is <span>${dayDifference}</span> days <br>
                          Difference between the month is <span>${monthDifference}</span> months <br>
